1e5df0b314ff598fa2ccd8399b1addcbc8cc0d1c
Johbra96 <61549336+Johbra96@users.noreply.github.com>
2020-12-08 17:30:47 +0100

    Sidebar functionality and design

---
+++ js/sidebar.js
@@ -0,0 +1,15 @@
+function batteryDropDownContent(){
+    document.getElementById("battery__dropdown").classList.toggle("show");
+}
+
+function pcbDropDownContent(){
+    document.getElementById("pcb__dropdown").classList.toggle("show");
+}
+
+function solarPanelDropDownContent(){
+    document.getElementById("solarPanel__dropdown").classList.toggle("show");
+}
+
+function ledDropDownContent(){
+    document.getElementById("led__dropdown").classList.toggle("show");
+}
